6b7229aebdce5f642e13c42b6feffdaea65598bc
marcacohen <marcacohen@gmail.com>
2019-05-06 22:56:26 +0100

    add support for loadrun.io

--- hello.go
+++ hello.go
@@ -3,13 +3,22 @@ import (
   "net/http"
   "strings"
 )
+
+var token = "loaderio-3c7cdedd29a59ff8714d4bf41f1c1870"
+
+func sendToken(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte(token))
+}
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
   message = "Hello " + message
   w.Write([]byte(message))
 }
+
 func main() {
+  http.HandleFunc("/"+token+"/", sendToken)
   http.HandleFunc("/", sayHello)
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
