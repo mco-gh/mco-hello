e0636229560e46fc4043973de847bd39e2efb64b
marcacohen <marcacohen@gmail.com>
2020-05-07 16:38:00 +0100

    simplify and add color support

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 import (
+  "fmt"
+  "math/rand"
   "net/http"
   "strings"
+  "time"
 )
 
 var token = "loaderio-b1563d0e1a489bdfd2b21cc76d9b3c22"
@@ -10,13 +13,16 @@ func sendToken(w http.ResponseWriter, r *http.Request) {
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Hello there " + message + "!!!"
+  path := strings.TrimPrefix(r.URL.Path, "/")
+  random := rand.Intn(16777215)
+  color := fmt.Sprintf("%6.6x", random)
+  style := "style=\"background-color:#" + color + "\""
+  message := "<h1 " + style + ">Hello " + path + "!!!</h1>"
   w.Write([]byte(message))
 }
 
 func main() {
+  rand.Seed(time.Now().UTC().UnixNano())
   http.HandleFunc("/"+token+"/", sendToken)
   http.HandleFunc("/", sayHello)
   if err := http.ListenAndServe(":8080", nil); err != nil {
