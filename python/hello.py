0104d796dbb0cfd1aa0df4cfd739f7e533f1b412
Melanie Warrick <warrick@google.com>
2020-06-15 11:30:23 -0700

    Fix the color issue, add error reporting and clean up.

--- python/hello.py
+++ python/hello.py
@@ -1,41 +1,29 @@
 from flask import Flask, request, escape, render_template
+from werkzeug.exceptions import HTTPException, BadRequest
+
 import random
 
-# To build docker in the local directory use the following and pass a name to call for run
-# docker build --tag name . 
-# If no name passed then not the pid and use that to run
-# Run with 
-# docker run -d -p 8080:8080 [name or id]
-# Stop
-# docker stop [id - first 4]
+'''
+Notes on how to run locally:
 
+To build docker in the local directory use the following and pass a name to call for run:
+docker build --tag name . 
+
+If no name passed then note the pid and use that to run:
+docker run -d -p 8080:8080 [name or id]
+
+Stop the instance with:
+docker stop [id - first 4]
+
+Deploy to registry
+'''
 app = Flask(__name__)
-
-# Fix for the load.io token - ?
-#   http.HandleFunc("/"+token+"/", sendToken)
-# def send_token(request) 
-#   token = "loaderio-b1563d0e1a489bdfd2b21cc76d9b3c22"
-#   w.Write([]byte(token))
-
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/<name>', methods=['POST', 'GET'])
 def say_hello(name='World'):
-  num = random.randint(1,1000)
 
-  # if request:
-  #   request_json = request.get_json(silent=True)
-  #   request_args = request.args
-  #   if request_json and 'name' in request_json:
-  #       name = request_json['name']
-  #       print(request_json)
-  #   elif request_args and 'name' in request_args:
-  #       name = request_args['name']
-  #       print(request_args)
-  # print(request)
-
-
-  color = random.randint(1,16777215)
+  color = "%06x" % random.randint(0, 0xFFFFFF)
   style = "style=\"background-color:#" + str(color) + "\""
   combined_html = "<h1 " + style + ">Hello " + name + "!!!</h1>"
 
@@ -51,14 +39,24 @@ def not_found(error):
   #Option to use template 
   #return render_template('home.hml')
 
-#   if err := http.ListenAndServe(":8080", nil); err != nil 
-#     panic(err)
-  
+@app.errorhandler(BadRequest)
+def handle_bad_request(e):
+    return 'bad request!', 400
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 if __name__ == '__main__':
 
-    # option 2
-    # app.add_url_rule('/say_hello', 'say_hello', say_hello, methods=['POST', 'GET'], defaults={'request': request})
-
     app.run(host='0.0.0.0', port=8080, use_reloader=True, debug=True)
-
