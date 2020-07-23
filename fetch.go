7c513f4b67857bcd3e61028004cf33f08fdba81e
marcacohen <marcacohen@gmail.com>
2020-07-23 14:36:14 +0100

    add fetch and concurrent fetch

---
+++ fetch.go
@@ -0,0 +1,33 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	for _, url := range os.Args[1:] {
+		reqStart := time.Now()
+		resp, err := http.Get(url)
+		reqSecs := time.Since(reqStart).Seconds()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		//fmt.Printf("%s", b)
+		nbytes := len(b)
+		fmt.Printf("%.2fs %7d %s\n", reqSecs, nbytes, url)
+	}
+	fmt.Printf("\n%.2fs elapsed\n", time.Since(start).Seconds())
+}
